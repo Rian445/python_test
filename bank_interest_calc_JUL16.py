a4462a618218242807454a0423f00897fc37a3cf
Rian445 <rianislam445@gmail.com>
2025-07-16 14:25:43 +0600

    Added new file and updates

--- bank_interest_calc_JUL16.py
+++ bank_interest_calc_JUL16.py
@@ -6,9 +6,10 @@ millionire_time = 0
 while True:
     principle = float(input("Enter the principal amount : "))
     print("\n")
-    if principle < 0:
+    while principle < 0:
         print("Principal amount cannot be negative. Please enter a valid amount.\n")
         principle = float(input("Enter the principal amount : "))
+        print("\n")
     else:
         break
 
@@ -16,18 +17,20 @@ while True:
 while True:
     rate = float(input("Enter the rate of interest : "))
     print("\n")
-    if rate < 0:
+    while rate < 0:
         print("Rate of interest cannot be negative. Please enter a valid rate.\n")
         rate = float(input("Enter the rate of interest : "))
+        print("\n")
     else:
         break
 
 while True:
     time = float(input("Enter the time in years : "))
     print("\n")
-    if time < 0:
+    while time < 0:
         print("Time cannot be negative. Please enter a valid time.\n")
         time = float(input("Enter the time in years : "))
+        print("\n")
     else:
         break
 
@@ -37,7 +40,6 @@ millionire_time = math.log(1_000_000 / principle) / math.log(1 + rate / 100)
 
 if total_ammount >= 1000000 and principle < 1000000 and millionire_time >= 1:
 
-    millionire_time = (time*1000000) / total_ammount
     print(f"Total amount after {time} year/s is: ${total_ammount:.2f}\n")
     print(
         f"You will become a millionaire after {millionire_time:.2f} year/s!\n")
